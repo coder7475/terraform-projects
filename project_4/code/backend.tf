c511ea2a90d50ff1e94d48777f8dbeab5ed1e4e2
coder7475 <robiulhossain7475@gmail.com>
2026-03-07 17:06:28 +0600

    feat: add Elastic Beanstalk IAM roles, application and S3 bucket for blue-green deployment
    - Add IAM role for EC2 instances with WebTier, WorkerTier and Multicontainer Docker policies
    - Add IAM role for Elastic Beanstalk service with Enhanced Health and Managed Updates policies
    - Create instance profile for EC2 instances
    - Add Elastic Beanstalk application resource
    - Add S3 bucket for storing application versions with public access blocking
    - Update backend state key to project_4
    - Add new variables: app_name, solution_stack_name, instance_type, and tags

--- project_4/code/backend.tf
+++ project_4/code/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-state-bootstrap-5aa1a0fb"
-    key    = "project_3/terraform.tfstate"
+    key    = "project_4/terraform.tfstate"
     region = "ap-southeast-1"
     use_lockfile = true
     encrypt = true
--- project_4/code/variables.tf
+++ project_4/code/variables.tf
@@ -4,13 +4,38 @@ variable "aws_region" {
   description = "AWS region for resources"
 }
 
-variable "bucket_name" {
+variable "app_name" {
   type = string
-  default = "project-1-bucket-1"
+  default = "bg-deployment"
 }
 
 variable "env" {
   type = string
   default = "development"
 }
+
+variable "solution_stack_name" {
+  description = "Elastic Beanstalk solution stack name (platform)"
+  type        = string
+  # Node.js 20 running on 64bit Amazon Linux 2023
+  default = "64bit Amazon Linux 2023 v6.6.8 running Node.js 20"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for Elastic Beanstalk environments"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Project     = "BlueGreenDeployment"
+    Environment = "Demo"
+    ManagedBy   = "Terraform"
+  }
+}
+
+
 
